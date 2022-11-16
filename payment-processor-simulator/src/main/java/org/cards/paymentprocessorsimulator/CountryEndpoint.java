046b532e1f848668c699ddd59f9dbc3e9562fcd2
jbprek <jbprek@gmail.com>
2022-11-16 21:51:29 +0000

    Project Initialization

---
+++ payment-processor-simulator/src/main/java/org/cards/paymentprocessorsimulator/CountryEndpoint.java
@@ -0,0 +1,31 @@
+package org.cards.paymentprocessorsimulator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import org.cards.services.payment_processor.GetCountryRequest;
+import org.cards.services.payment_processor.GetCountryResponse;
+
+
+@Endpoint
+public class CountryEndpoint {
+	private static final String NAMESPACE_URI = "http://cards.org/services/payment-processor";
+
+	private CountryRepository countryRepository;
+
+	@Autowired
+	public CountryEndpoint(CountryRepository countryRepository) {
+		this.countryRepository = countryRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+	@ResponsePayload
+	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+		GetCountryResponse response = new GetCountryResponse();
+		response.setCountry(countryRepository.findCountry(request.getName()));
+
+		return response;
+	}
+}
